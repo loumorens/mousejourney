baf90b295716b9756472b49edaa655ae8cc6fe99
Lou Morens <lou.morens@lisaae.com>
2025-07-12 11:02:54 +0200

    update scene to use cinemachine with third person camera

--- Assets/MouseJourney/Scripts/Internal/FollowPlayer.cs
+++ Assets/MouseJourney/Scripts/Internal/FollowPlayer.cs
@@ -4,6 +4,7 @@ public class FollowPlayer : MonoBehaviour
 {
     // ref to the player, the vehicle
     public GameObject player;
+    public float damping = 1;
 
     //offset camera
     public Vector3 offset = new Vector3(-0.3f, 0.32f, -0.06f);
@@ -11,7 +12,8 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame after the update method : use to slooth the camera movement behind the vehicle
     void LateUpdate()
     {
+        float angleY = player.transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0,angleY,0);
         transform.position = player.transform.position + offset;
-        transform.rotation = player.transform.rotation;
     }
 }
